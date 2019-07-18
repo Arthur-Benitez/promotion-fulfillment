add2d62360fc037b68cba9788bfc8e99ebc9fe08
Arthur Benitez <arturodbl@outlook.com>
2019-07-18 10:57:06 -0500

    feat(notif): Guardar el mensaje desplegado en el archivo del usuario.

--- modules/notifications.R
+++ modules/notifications.R
@@ -1,12 +1,32 @@
+#Funciones
+save_title <- function(message_data, gl, credentials) {
+  user_notifications_path <- file.path(gl$app_deployment_environment, 'notifications', 'users')
+  user_file <- sprintf('%s/%s.csv', user_notifications_path, credentials$user)
+  
+  if (!dir.exists(user_notifications_path)) {
+    dir.create(user_notifications_path, recursive = TRUE)
+  }
+  
+  write_csv(message_data, path = user_file, na = '', append = ifelse(file.exists(user_file), TRUE, FALSE))
+}
+
 # Server ------------------------------------------------------------------
 notificationsServer <- function(input, output, session, credentials) {
+  message = 'instructions.html'
   showModal(modalDialog(
     size = 'l',
     easyClose = TRUE,
     title = 'Instrucciones',
-    includeHTML('dev/notifications/messages/instructions.html'),
+    includeHTML(sprintf('dev/notifications/messages/%s', message)),
     footer = modalButton(lang$ok)
   ))
+  
+  message_data <- tibble(message) %>% 
+    mutate(
+      view_time = format(Sys.time(), "%x %X", tz = 'America/Mexico_City'),
+      date = format(Sys.time(), "%x")
+    )
+  save_title(message_data = message_data, gl = gl, credentials = credentials)
 }
 
 # UI ----------------------------------------------------------------------
