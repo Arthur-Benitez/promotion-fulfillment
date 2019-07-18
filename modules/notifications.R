5e2cd912162246c1bf99d44bf6a61dd09afa94dd
Arthur Benitez <arturo.benitez@walmart.com>
2019-07-18 12:02:37 -0500

    fix(notif): Desplegar solo los mensajes que no haya visto el usuario.

--- modules/notifications.R
+++ modules/notifications.R
@@ -1,32 +1,44 @@
-#Funciones
-save_title <- function(message_data, gl, credentials) {
-  user_notifications_path <- file.path(gl$app_deployment_environment, 'notifications', 'users')
-  user_file <- sprintf('%s/%s.csv', user_notifications_path, credentials$user)
-  
+# Functions ---------------------------------------------------------------
+save_title <- function(message_data, user_file, user_notifications_path) {
   if (!dir.exists(user_notifications_path)) {
     dir.create(user_notifications_path, recursive = TRUE)
   }
-  
   write_csv(message_data, path = user_file, na = '', append = ifelse(file.exists(user_file), TRUE, FALSE))
 }
 
 # Server ------------------------------------------------------------------
 notificationsServer <- function(input, output, session, credentials) {
-  message = 'instructions.html'
-  showModal(modalDialog(
-    size = 'l',
-    easyClose = TRUE,
-    title = 'Instrucciones',
-    includeHTML(sprintf('dev/notifications/messages/%s', message)),
-    footer = modalButton(lang$ok)
-  ))
+  deploy_file <- file.path(gl$app_deployment_environment, 'notifications', 'messages', 'deploy.csv')
+  user_notifications_path <- file.path(gl$app_deployment_environment, 'notifications', 'users')
+  user_file <- sprintf('%s/%s.csv', user_notifications_path, 'sam')
+  # credentials()$user
+  all_messages <- read_csv(deploy_file) %>% 
+    select(message)
+  if (file.exists(user_file)) {
+    user_messages <- read_csv(user_file)
+    message <- all_messages %>% 
+      filter(!(message %in% user_messages$message))
+  } else {
+    message <- all_messages
+  }
+
   
-  message_data <- tibble(message) %>% 
-    mutate(
-      view_time = format(Sys.time(), "%x %X", tz = 'America/Mexico_City'),
-      date = format(Sys.time(), "%x")
-    )
-  save_title(message_data = message_data, gl = gl, credentials = credentials)
+  if (nrow(message) > 0 && is.data.frame(message)) {
+    showModal(modalDialog(
+      size = 'l',
+      easyClose = TRUE,
+      title = 'Anuncios',
+      includeHTML(sprintf('dev/notifications/messages/%s', message[1,1])),
+      footer = modalButton(lang$ok)
+    ))
+    
+    message_data <- message %>% 
+      mutate(
+        view_time = format(Sys.time(), "%x %X", tz = 'America/Mexico_City'),
+        date = format(Sys.time(), "%x")
+      )
+    save_title(message_data, user_file, user_notifications_path) 
+  }
 }
 
 # UI ----------------------------------------------------------------------
