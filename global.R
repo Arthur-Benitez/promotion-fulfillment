9359d6764c08cdddaaba5932b9be2967efeba7da
Arthur Benitez <arturodbl@outlook.com>
2019-10-30 14:24:37 -0600

    release(1.5.2)
    Se mejoraron las listas de tiendas en el ejemplo.
    Se mejoraron los defaults del módulo de usage.
    Se incluye el reporte de seguimiento.

--- global.R
+++ global.R
@@ -115,8 +115,8 @@ gl <- list(
       'dev'
     }
   },
-  app_version = '1.5.1',
-  app_version_date = '2019-09-19',
+  app_version = '1.5.2',
+  app_version_date = '2019-10-30',
   ## Compute promotions
   cols = tryCatch({
     read_tsv('data/column-info.txt', col_types = 'ccllccncc')
