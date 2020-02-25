c8cdccee603bc9618bb76a50d23cb0785f8a1969
Felipe Gerard <felipe.gerard@walmart.com>
2020-02-25 21:36:03 +0000

    feat(update_rrp): Added logs and structure to avoid crashing and to help debug.

--- utils/update-rrp.R
+++ utils/update-rrp.R
@@ -2,6 +2,8 @@
 ## This is necessary because a bug in the Element platform causes an error to prevent queries from being made in Teradata after a query using the db2 production connector has been previously used to run a query.
 
 library(tidyverse)
+library(futile.logger)
+library(jsonlite)
 
 app_deployment_environment <- getArgument('app_deployment_environment', 'PARAMS')
 
@@ -11,30 +13,73 @@ if (app_deployment_environment == 'prod') {
   setwd('/home/rstudio/dev/DEV-promo-fulfillment-app')
 }
 
+flog.info(toJSON(list(
+  message = 'WORKING DIRECTORY SET',
+  details = list(
+    app_deployment_environment = app_deployment_environment,
+    working_dir = getwd()
+  )
+)))
+
 functions_path <- 'utils/functions.R'
 rrp_database_path <- 'data/item-rrp-sync-result.rds'
 rrp_query_path <- 'sql/item_rrp_sync.sql'
 connector <- 'db2-production-connector'
 rrp_backups_n <- 5
-source(functions_path)
+
+# This is necessary because the "backup_file" function is required to save a copy of the database.
+tryCatch({
+  source(functions_path)  
+}, error = function(e){
+  flog.error(toJSON(list(
+    message = 'FUNCTIONS SOURCE NOT FOUND',
+    details = list(
+      functions_path = functions_path,
+      working_dir = getwd()
+    )
+  )))
+})
 
 query <- readLines(rrp_query_path) %>% 
   paste(collapse = '\n')
-res <- mlutils::dataset.load(
-  name = connector,
-  query = query
-  ) %>% 
-  as_tibble() %>% 
-  set_names(tolower(names(.))) %>% 
-  rename(
-    old_nbr = item_nbr,
-    rrp_ind = retail_ready_pack_ind
+res <- purrr::safely(mlutils::dataset.load)(name = connector, query = query)
+
+if (is.data.frame(res$result)) {
+  res <- res$result %>%
+    mutate_if(is.factor, as.character) %>% 
+    as_tibble() %>% 
+    set_names(tolower(names(.))) %>% 
+    rename(
+      old_nbr = item_nbr,
+      rrp_ind = retail_ready_pack_ind
+    )
+} else {
+  res <- paste(res$error, collapse = ' ')
+  flog.error(toJSON(list(
+    message = 'QUERY FAILED',
+    details = list(
+      error_msg = substr(res, 1, 200),
+      query = substr(query, 1, 300)
+    )
+  )))
+  stop(res)
+}
+
+tryCatch({
+  backup_file(
+    res,
+    folder = dirname(rrp_database_path),
+    complete_file_name = basename(rrp_database_path),
+    preserved_backups = rrp_backups_n
   )
-backup_file(
-  res,
-  folder = dirname(rrp_database_path),
-  complete_file_name = basename(rrp_database_path),
-  preserved_backups = rrp_backups_n
-)
+}, error = function(e){
+  flog.error(toJSON(list(
+    message = 'BACKUP NOT SUCCESSFUL',
+    details = list(
+      data = head(res, 2),
+      path = rrp_database_path
+    )
+  )))
+})
 saveRDS(res, rrp_database_path)
 
