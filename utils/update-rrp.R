e405030a0046ff2e83873471962571802ad35286
Felipe Gerard <felipe.gerard@walmart.com>
2020-02-25 20:39:10 +0000

    fix(update_rrp): Add extra info to allow the code run as a standalone.

--- utils/update-rrp.R
+++ utils/update-rrp.R
@@ -1,14 +1,22 @@
 ## This script is intended to run in an Element automated workflow with the objective of automatically and daily, update the RRP & GS1 Sync status database.
 ## This is necessary because a bug in the Element platform causes an error to prevent queries from being made in Teradata after a query using the db2 production connector has been previously used to run a query.
 
-query <- readLines('sql/item_rrp_sync.sql') %>% 
+library(tidyverse)
+
+working_dir <- '/home/rstudio/dev/DEV-promo-fulfillment-app'
+functions_path <- 'utils/functions.R'
+rrp_database_path <- file.path(working_dir, 'data/item-rrp-sync-result.rds')
+rrp_query_path <- 'sql/item_rrp_sync.sql'
+connector <- 'db2-production-connector'
+rrp_backups_n <- 5
+source(file.path(working_dir, functions_path))
+
+query <- readLines(file.path(working_dir, rrp_query_path)) %>% 
   paste(collapse = '\n')
-res <- sql_query(
-  ch = NULL,
-  connector =  'db2-production-connector',
-  query = query,
-  stringsAsFactors = FALSE
-) %>% 
+res <- mlutils::dataset.load(
+  name = connector,
+  query = query
+  ) %>% 
   as_tibble() %>% 
   set_names(tolower(names(.))) %>% 
   rename(
@@ -17,9 +25,9 @@ res <- sql_query(
   )
 backup_file(
   res,
-  folder = dirname(gl$rrp_sync_database),
-  complete_file_name = basename(gl$rrp_sync_database),
-  preserved_backups = gl$rrp_sync_backups_n
+  folder = dirname(rrp_database_path),
+  complete_file_name = basename(rrp_database_path),
+  preserved_backups = rrp_backups_n
 )
-saveRDS(res, gl$rrp_sync_database)
+saveRDS(res, rrp_database_path)
 
